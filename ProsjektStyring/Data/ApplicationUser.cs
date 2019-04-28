e21e6dfdb9c49141fdcd9649a499d3c1990d7eb7
Allan Arnesen <allan.arnesen@gmail.com>
2019-04-28 11:11:32 +0200

    -

--- ProsjektStyring/Data/ApplicationUser.cs
+++ ProsjektStyring/Data/ApplicationUser.cs
@@ -8,11 +8,17 @@ namespace ProsjektStyring.Data
 {
     public class ApplicationUser : IdentityUser
     {
+        [PersonalData]
         public DateTime RegistrationDate { get; set; }
+        [PersonalData]
         public string FirstName { get; set; }
+        [PersonalData]
         public string LastName { get; set; }
+        [PersonalData]
         public string FullName { get { return string.Format("{0} {1}", FullName, LastName); } }
+        [PersonalData]
         public bool Active { get; set; }
+        [PersonalData]
         public string EmployeeId { get; set; }
 
 
